eecbf4c0a0e73398bc2b8a500be077a2fa82f6b1
Vraj Shah <vraj0410shah@gmail.com>
2025-03-26 13:02:42 +0100

    created a user friendly input button for making it more dynamic

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function createcard(title, channel, views, date_uploaded) {
+let details = [];
+
+
+
+function createcard(details) {
     const cointainer = document.body.querySelector(".cointainer");
     let view_str;
-    if (views<1000){
-        view_str = views;
+    if (details[2] < 1000) {
+        view_str = details[2];
     }
-    else if (views<1000000){
-        view_str = views/1000 + "K";
+    else if (details[2] < 1000000) {
+        view_str = details[2] / 1000 + "K";
     }
-    else{
-        view_str = views/1000000 + "M";
+    else {
+        view_str = details[2] / 1000000 + "M";
     }
     let html =
         `<div class="card">
@@ -16,10 +20,42 @@ function createcard(title, channel, views, date_uploaded) {
                 <img src="https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw" alt="">
             </div>
             <div class="text">
-                <h2>${title}</h2>
-                <p>${channel} . ${view_str} views . ${date_uploaded}</p>
+                <h2>${details[0]}</h2>
+                <p>${details[1]} . ${view_str} views . ${details[3]}</p>
             </div>
         </div>`
-        cointainer.innerHTML += html;
+    cointainer.innerHTML += html;
 }
 
+const dataInput = document.querySelector(".data-input");
+
+document.querySelector(".add_btn").addEventListener('click', addTask);
+
+dataInput.addEventListener('keypress', (e) => e.key === 'Enter' && addTask());
+
+function addTask() {
+
+    const dataText = dataInput.value.trim();
+    if (!dataText) return alert("Please enter a data!");
+
+    details.push(dataText); // Store user input
+    dataInput.value = ""; // Clear input field
+    dataInput.focus(); // Keep input field active
+    if(details.length === 0){
+        dataInput.placeholder = "enter title";
+    }
+    if(details.length === 1){
+        dataInput.placeholder = "enter channel";
+    }
+    if(details.length === 2){
+        dataInput.placeholder = "enter views";
+    }
+    if(details.length === 3){
+        dataInput.placeholder = "enter time ago";
+    }
+    if (details.length === 4) { // Only create a card when 4 inputs are collected
+        createcard(details);
+        details = []; // Reset for the next entry
+    }
+
+}
